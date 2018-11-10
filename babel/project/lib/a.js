00fd047b326f18810bbca7f388680ccde5af37da
吴建鹏 <jianpeng.wu@geely.com>
2018-11-10 14:57:36 +0800

    project config

---
+++ babel/project/lib/a.js
@@ -0,0 +1,11 @@
+"use strict";
+
+require("core-js/modules/web.dom.iterable");
+
+require("core-js/modules/es6.array.iterator");
+
+require("core-js/modules/es6.string.iterator");
+
+require("core-js/modules/es6.map");
+
+var b = new Map();
